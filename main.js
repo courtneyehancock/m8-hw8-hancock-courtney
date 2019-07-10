f3b2c02954c47cf0a060b08d497d0e95106a705a
Courtney Hancock <ceh.courtney@gmail.com>
2019-07-09 19:42:21 -0700

    Edit javascript to add function

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 const myBoxes = document.getElementsByClassName('block');
 
 // The Functionality
-
+function loopThrough() {
+  console.log(this);
+};
 
 // The Event Listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
